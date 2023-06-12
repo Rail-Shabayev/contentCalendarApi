297a20ca01818223e605d5f3c7e29055cf3c2cfd
Rail-Shabayev <rail.shabayev@mail.ru>
2023-06-12 20:12:30 +0400

    asdf

---
+++ src/main/java/com/rail/calendar/repo/ContentRepository.java
@@ -0,0 +1,8 @@
+package com.rail.calendar.repo;
+
+import com.rail.calendar.model.Content;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface ContentRepository extends ListCrudRepository<Content, Integer> {
+
+}
